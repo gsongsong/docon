07ec4a6b54a5d99a8abf45269ac21192b8108bb4
Seokseong Jeon <sjeon87@gmail.com>
2020-02-05 22:38:46 +0900

    v1.0.0

--- docon/Program.cs
+++ docon/Program.cs
@@ -1,15 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace docon
 {
   class Program
   {
+    static readonly string FormatTxt = "txt";
+    static readonly string FormatHtm = "htm";
+    static readonly int EncodingUtf8 = 65001;
+
     static void Main(string[] args)
     {
+      CheckArgs(args);
+      var inputFile = args[0];
+      CheckInputFile(inputFile);
+      var nameWoExt = Path.GetFileNameWithoutExtension(inputFile);
+      var outputFormat = args[1];
+      CheckOutputFormat(outputFormat);
+      var currentDirectory = Directory.GetCurrentDirectory();
+      var outputFile = Path.Combine(currentDirectory, nameWoExt + "." + outputFormat);
+      var wordApp = new Word.Application();
+      var doc = wordApp.Documents.Open(inputFile);
+      var format = outputFormat == FormatTxt ? WdSaveFormat.wdFormatText : WdSaveFormat.wdFormatFilteredHTML;
+      doc.SaveAs2(outputFile, format, Encoding: EncodingUtf8);
+      doc.Close();
+      wordApp.Quit();
+    }
+
+    static void CheckArgs(string[] args)
+    {
+      if (args.Length != 2)
+      {
+        Console.WriteLine("Input file and output format must be specified. Exit.");
+        Environment.Exit(-1);
+      }
+    }
+
+    static void CheckInputFile(string inputFile)
+    {
+      if (!File.Exists(inputFile))
+      {
+        Console.WriteLine("Input file does not exist. Exit.");
+        Environment.Exit(-1);
+      }
+    }
+
+    static void CheckOutputFormat(string outputFormat)
+    {
+      if (outputFormat != FormatTxt && outputFormat != FormatHtm)
+      {
+        Console.WriteLine("Output format must be either \"txt\" or \"htm\"");
+        Environment.Exit(-1);
+      }
     }
   }
 }
